bea12242a11e198efda3ae51c954724ce9e15d6d
neledov <neledov@gmail.com>
2024-12-03 10:11:19 +0100

    new saml namespaces added for proper saml request type detection

--- static/js/utils/saml.js
+++ static/js/utils/saml.js
@@ -24,27 +24,44 @@ export const parseSamlXml = (xmlString) => {
 };
 
 const getSamlType = (xmlDoc) => {
+    if (xmlDoc.getElementsByTagNameNS('urn:oasis:names:tc:SAML:2.0:protocol', 'Response').length > 0) return 'Response';
+    if (xmlDoc.getElementsByTagNameNS('urn:oasis:names:tc:SAML:2.0:protocol', 'Request').length > 0) return 'Request';
+    if (xmlDoc.getElementsByTagNameNS('urn:oasis:names:tc:SAML:2.0:protocol', 'AuthnRequest').length > 0) return 'AuthnRequest';
     if (xmlDoc.getElementsByTagName('samlp:Response').length > 0) return 'Response';
+    if (xmlDoc.getElementsByTagName('saml2p:Response').length > 0) return 'Response';
     if (xmlDoc.getElementsByTagName('samlp:Request').length > 0) return 'Request';
     if (xmlDoc.getElementsByTagName('samlp:AuthnRequest').length > 0) return 'AuthnRequest';
     return 'Unknown';
 };
 
 const extractIssuer = (xmlDoc) => {
-    const issuerNode = xmlDoc.getElementsByTagName('saml:Issuer')[0];
+    const issuerNode = 
+        xmlDoc.getElementsByTagNameNS('urn:oasis:names:tc:SAML:2.0:assertion', 'Issuer')[0] ||
+        xmlDoc.getElementsByTagName('saml:Issuer')[0] ||
+        xmlDoc.getElementsByTagName('saml2:Issuer')[0];
     return issuerNode ? issuerNode.textContent : null;
 };
 
 const extractAttributes = (xmlDoc) => {
     const attributes = [];
-    const attrStatements = xmlDoc.getElementsByTagName('saml:AttributeStatement');
+    const attrStatements = [
+        ...Array.from(xmlDoc.getElementsByTagNameNS('urn:oasis:names:tc:SAML:2.0:assertion', 'AttributeStatement')),
+        ...Array.from(xmlDoc.getElementsByTagName('saml:AttributeStatement')),
+        ...Array.from(xmlDoc.getElementsByTagName('saml2:AttributeStatement'))
+    ];
     
     for (const statement of attrStatements) {
-        const attrs = statement.getElementsByTagName('saml:Attribute');
+        const attrs = [
+            ...Array.from(xmlDoc.getElementsByTagNameNS('urn:oasis:names:tc:SAML:2.0:assertion', 'Attribute')),
+            ...Array.from(statement.getElementsByTagName('saml:Attribute')),
+            ...Array.from(statement.getElementsByTagName('saml2:Attribute'))
+        ];
         for (const attr of attrs) {
             attributes.push({
                 name: attr.getAttribute('Name'),
                 values: Array.from(attr.getElementsByTagName('saml:AttributeValue'))
+                           .concat(Array.from(attr.getElementsByTagName('saml2:AttributeValue')))
+                           .concat(Array.from(attr.getElementsByTagNameNS('urn:oasis:names:tc:SAML:2.0:assertion', 'AttributeValue')))
                            .map(value => value.textContent)
             });
         }
@@ -53,33 +70,56 @@ const extractAttributes = (xmlDoc) => {
 };
 
 const extractConditions = (xmlDoc) => {
-    const conditionsNode = xmlDoc.getElementsByTagName('saml:Conditions')[0];
+    const conditionsNode = 
+        xmlDoc.getElementsByTagNameNS('urn:oasis:names:tc:SAML:2.0:assertion', 'Conditions')[0] ||
+        xmlDoc.getElementsByTagName('saml:Conditions')[0] ||
+        xmlDoc.getElementsByTagName('saml2:Conditions')[0];
     if (!conditionsNode) return null;
 
     return {
         notBefore: conditionsNode.getAttribute('NotBefore'),
         notOnOrAfter: conditionsNode.getAttribute('NotOnOrAfter'),
-        audiences: Array.from(conditionsNode.getElementsByTagName('saml:AudienceRestriction'))
-                      .map(ar => ar.getElementsByTagName('saml:Audience')[0]?.textContent)
+        audiences: Array.from(conditionsNode.getElementsByTagNameNS('urn:oasis:names:tc:SAML:2.0:assertion', 'AudienceRestriction'))
+                      .concat(Array.from(conditionsNode.getElementsByTagName('saml:AudienceRestriction')))
+                      .concat(Array.from(conditionsNode.getElementsByTagName('saml2:AudienceRestriction')))
+                      .map(ar => {
+                          const audience = ar.getElementsByTagNameNS('urn:oasis:names:tc:SAML:2.0:assertion', 'Audience')[0] ||
+                                         ar.getElementsByTagName('saml:Audience')[0] ||
+                                         ar.getElementsByTagName('saml2:Audience')[0];
+                          return audience?.textContent;
+                      })
                       .filter(Boolean)
     };
 };
 
 const extractAuthStatements = (xmlDoc) => {
-    return Array.from(xmlDoc.getElementsByTagName('saml:AuthnStatement'))
-        .map(statement => ({
-            authnInstant: statement.getAttribute('AuthnInstant'),
-            sessionIndex: statement.getAttribute('SessionIndex'),
-            authnContext: statement.getElementsByTagName('saml:AuthnContext')[0]
-                ?.getElementsByTagName('saml:AuthnContextClassRef')[0]?.textContent
-        }));
+    const statements = [
+        ...Array.from(xmlDoc.getElementsByTagNameNS('urn:oasis:names:tc:SAML:2.0:assertion', 'AuthnStatement')),
+        ...Array.from(xmlDoc.getElementsByTagName('saml:AuthnStatement')),
+        ...Array.from(xmlDoc.getElementsByTagName('saml2:AuthnStatement'))
+    ];
+    
+    return statements.map(statement => ({
+        authnInstant: statement.getAttribute('AuthnInstant'),
+        sessionIndex: statement.getAttribute('SessionIndex'),
+        authnContext: (
+            statement.getElementsByTagNameNS('urn:oasis:names:tc:SAML:2.0:assertion', 'AuthnContextClassRef')[0] ||
+            statement.getElementsByTagName('saml:AuthnContextClassRef')[0] ||
+            statement.getElementsByTagName('saml2:AuthnContextClassRef')[0]
+        )?.textContent
+    }));
 };
 
 const extractAssertions = (xmlDoc) => {
-    return Array.from(xmlDoc.getElementsByTagName('saml:Assertion'))
-        .map(assertion => ({
-            id: assertion.getAttribute('ID'),
-            issueInstant: assertion.getAttribute('IssueInstant'),
-            version: assertion.getAttribute('Version')
-        }));
+    const assertions = [
+        ...Array.from(xmlDoc.getElementsByTagNameNS('urn:oasis:names:tc:SAML:2.0:assertion', 'Assertion')),
+        ...Array.from(xmlDoc.getElementsByTagName('saml:Assertion')),
+        ...Array.from(xmlDoc.getElementsByTagName('saml2:Assertion'))
+    ];
+    
+    return assertions.map(assertion => ({
+        id: assertion.getAttribute('ID'),
+        issueInstant: assertion.getAttribute('IssueInstant'),
+        version: assertion.getAttribute('Version')
+    }));
 };
